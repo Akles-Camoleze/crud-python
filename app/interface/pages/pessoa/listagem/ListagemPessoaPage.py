41f24cefa3d4f72495ca479bef1735a1694573ef
Akles Pires Camoleze <akles@celulati.com.br>
2024-11-20 22:42:58 -0300

    [FEATURE] Ajuste no refresh da listagem

--- app/interface/pages/pessoa/listagem/ListagemPessoaPage.py
+++ app/interface/pages/pessoa/listagem/ListagemPessoaPage.py
@@ -1,3 +1,5 @@
+import sys
+
 from app.entities.pessoa.Pessoa import Pessoa
 from app.interface.providers.gtk.GtkProvider import GtkProvider
 from app.interface.services.pessoa.PessoaService import PessoaService
@@ -18,26 +20,16 @@ class ListagemPessoaPage(GtkProvider.Box):
 
         self.pack_start(header_grid, False, False, 0)
 
-        list_box = GtkProvider.ListBox()
-
-        list_box.foreach(lambda widget: list_box.remove(widget))
-
-        pessoas = PessoaService().find_all()
-
-        if pessoas:
-            for pessoa in pessoas:
-                self.create_row(list_box, pessoa)
-        else:
-            empty_label = GtkProvider.Label(label="Nenhuma pessoa encontrada.", hexpand=True, halign=GtkProvider.Align.CENTER)
-            list_box.add(empty_label)
-
-        self.pack_start(list_box, True, True, 0)
+        self.list_box = GtkProvider.ListBox()
+        self.pack_start(self.list_box, True, True, 0)
 
         add_button = GtkProvider.Button(label="Adicionar")
         add_button.connect("clicked", self.on_add_clicked)
         self.pack_start(add_button, False, False, 0)
 
         self.modal = None
+
+        self.update_list()
 
     def create_row(self, list_box, pessoa):
         row = GtkProvider.ListBoxRow()
@@ -70,9 +62,9 @@ class ListagemPessoaPage(GtkProvider.Box):
         list_box.add(row)
 
     def on_delete_clicked(self, button, pessoa_id):
-        print(f"Excluindo pessoa com ID: {pessoa_id}")
         PessoaService().delete_by_id(pessoa_id)
         button.get_parent().get_parent().destroy()
+        self.update_list()
 
     def on_add_clicked(self, button):
         if not self.modal:
@@ -80,15 +72,12 @@ class ListagemPessoaPage(GtkProvider.Box):
         self.modal.show_all()
 
     def create_modal(self):
-        # Criando a janela modal
         self.modal = GtkProvider.Window(title="Adicionar Pessoa", modal=True)
 
-        # Criando o formulário
         form_grid = GtkProvider.Grid()
         form_grid.set_column_spacing(10)
         form_grid.set_row_spacing(10)
 
-        # Campos do formulário
         nome_label = GtkProvider.Label(label="Nome:")
         self.nome_entry = GtkProvider.Entry()
         form_grid.attach(nome_label, 0, 0, 1, 1)
@@ -109,7 +98,6 @@ class ListagemPessoaPage(GtkProvider.Box):
         form_grid.attach(cpf_label, 0, 3, 1, 1)
         form_grid.attach(self.cpf_entry, 1, 3, 1, 1)
 
-        # Botão para salvar a pessoa
         save_button = GtkProvider.Button(label="Salvar")
         save_button.connect("clicked", self.on_save_clicked)
         form_grid.attach(save_button, 1, 4, 1, 1)
@@ -126,8 +114,20 @@ class ListagemPessoaPage(GtkProvider.Box):
         PessoaService().save(pessoa)
 
         self.modal.hide()
+
         self.update_list()
 
     def update_list(self):
-        pass
+        self.list_box.foreach(lambda widget: self.list_box.remove(widget))
 
+        pessoas = PessoaService().find_all()
+
+        if pessoas:
+            for pessoa in pessoas:
+                self.create_row(self.list_box, pessoa)
+        else:
+            empty_label = GtkProvider.Label(label="Nenhuma pessoa encontrada.", hexpand=True, halign=GtkProvider.Align.CENTER)
+            self.list_box.add(empty_label)
+
+        self.list_box.queue_resize()
+        self.show_all()
